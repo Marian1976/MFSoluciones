541b2892b970b6c6b06d0b3e6e7ac7d3ca400bad
Marian1976 <147190361+Marian1976@users.noreply.github.com>
2026-05-12 12:54:55 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 // ══════════════════════════════════════════════════════
+// ══════════════════════════════════════════════════════
 //  MF SOLUCIONES — Configuración Global
 //  ► Editá SOLO este archivo para cambiar el túnel
 // ══════════════════════════════════════════════════════
 
-const API_BASE_URL = 'https://manila-calzone-relic.ngrok-free.dev';
+const API_BASE_URL = 'https://manila-calzone-relic.ngrok-free.dev ';
 
 // Alias usado internamente por portal.html
 const API = API_BASE_URL;
